fceb6aa50d1f631d4e23ff83b83eb1719396998d
Daniel Reyes <danielreyesicc@gmail.com>
2017-11-04 11:32:37 -0600

    Cocoapods installed.  SVProgressHUD Pod installed. Bridging header added.

---
+++ Aniflix/Controller/HomeController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeController.swift
+//  Aniflix
+//
+//  Created by Daniel Reyes Sánchez on 04/11/17.
+//  Copyright © 2017 Daniel Reyes Sánchez. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+class HomeController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ Aniflix/Utils/Aniflix-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Aniflix-Bridging-Header.h
+//  Aniflix
+//
+//  Created by Daniel Reyes Sánchez on 04/11/17.
+//  Copyright © 2017 Daniel Reyes Sánchez. All rights reserved.
+//
+
+#ifndef Aniflix_Bridging_Header_h
+#define Aniflix_Bridging_Header_h
+
+
+#endif /* Aniflix_Bridging_Header_h */
+
+#import <SVProgressHUD/SVProgressHUD.h>
